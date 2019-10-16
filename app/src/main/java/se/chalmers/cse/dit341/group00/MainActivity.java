094761f4b3167721e2d79c5834b8c2b056db5ac8
Fredrik93 <fredrikullman93@gmail.com>
2019-10-16 11:41:03 +0200

    add create room

--- app/src/main/java/se/chalmers/cse/dit341/group00/MainActivity.java
+++ app/src/main/java/se/chalmers/cse/dit341/group00/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -25,6 +27,12 @@ import se.chalmers.cse.dit341.group00.model.Room;
 
 public class MainActivity extends AppCompatActivity {
 
+    String text;
+
+    Button submitButton;
+
+    EditText roomName;
+
     // Field for parameter name
     public static final String HTTP_PARAM = "httpResponse";
 
@@ -32,16 +40,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView mRoomView = findViewById(R.id.roomTextView);
+
+
+
+        roomName = (EditText) findViewById(R.id.RoomNameInput);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text = roomName.getText().toString();
+
+                String url = getString(R.string.server_url) + "/api/rooms";
+                JSONObject postParams = new JSONObject();
+                try{
+                    postParams.put("name", text);
+                } catch (Exception e) {
+                    System.out.println(e.getStackTrace());
+                }
+
+                JsonObjectRequest jsonObjectReq = new JsonObjectRequest(Request.Method.POST, url, postParams,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                              mRoomView.setText("Room " + text + " Created ! ");
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+                queue.add(jsonObjectReq);
+            }
+        });
+
     }
 
     public void showPosts(View view){
       //  ScrollView mPostView = findViewById(R.id.postView);
         Intent intent = new Intent (this, PostActivity.class);
         startActivity(intent);
-
-
-
-
     }
 
     public void onClickNewRoom (View view) {
--- app/src/main/java/se/chalmers/cse/dit341/group00/model/Room.java
+++ app/src/main/java/se/chalmers/cse/dit341/group00/model/Room.java
@@ -1,6 +1,7 @@
 package se.chalmers.cse.dit341.group00.model;
 
 public class Room {
+    public String _id;
     public String name;
     public String [] users;
     public Post [] posts;
