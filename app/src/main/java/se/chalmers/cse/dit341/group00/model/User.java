bd25c816880dd59f6c18f71a426405ab8d8f3a6b
Fredrik93 <fredrikullman93@gmail.com>
2019-10-17 17:11:32 +0200

    add user

---
+++ app/src/main/java/se/chalmers/cse/dit341/group00/model/User.java
@@ -0,0 +1,16 @@
+package se.chalmers.cse.dit341.group00.model;
+
+public class User {
+
+    public String _id;
+    public String username;
+    public String age;
+    public String  email;
+    public String password;
+    public String posts;
+    public String achievements;
+
+    User(){
+
+    }
+}
